e739f845052cae8421ac00aab7d9c2b31c1c4169
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-25 20:29:38 +0545

    Move resolvers to a separate file

--- server/src/index.js
+++ server/src/index.js
@@ -7,14 +7,11 @@ const { ApolloServer } = require('apollo-server');
 // schemas
 const typeDefs = require('./schemas');
 
+// resolvers
+const resolvers = require('./resolvers');
+
 // env
 const { PORT } = require('./env_config');
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello World'
-  }
-};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
