bcc4cd9169d7607edb92a1cd85b6768560f37b29
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-25 20:24:29 +0545

    Create User type

--- server/src/schemas/index.js
+++ server/src/schemas/index.js
@@ -5,6 +5,25 @@ const typeDefs = gql`
   type Query {
     hello: String!
   }
+
+  enum Gender {
+    MALE
+    FEMALE
+    OTHER
+  }
+
+  "The User of the ystem"
+  type User {
+    id: ID!
+    email: String!
+    firstName: String!
+    middleName: String
+    lastName: String!
+    gender: Gender!
+
+    "Full name of the user"
+    name: String!
+  }
 `;
 
 module.exports = typeDefs;
