ea48e5900caacd287d76b5efa472019a534cac60
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-25 21:41:58 +0545

    Add a context to decode token from request

--- server/src/index.js
+++ server/src/index.js
@@ -15,6 +15,7 @@ const UserAPI = require('./dataSources/userAPI');
 
 // utils
 const prisma = require('./utils/prisma');
+const { decode } = require('./utils/token');
 
 // env
 const { PORT } = require('./env_config');
@@ -22,6 +23,26 @@ const { PORT } = require('./env_config');
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const _token = req?.headers?.authorization;
+
+    if (!_token) return {};
+    if (typeof _token !== 'string') return {};
+
+    const split = _token.split(' ');
+    if (split.length !== 2) return {};
+    if (split[0] !== 'Bearer') return {};
+
+    const userId = decode(split[1]);
+
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) return {};
+
+    user.password = '';
+
+    return { user };
+  },
   dataSources: () => ({
     userAPI: new UserAPI(prisma)
   })
