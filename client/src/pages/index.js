4e778cd59ccc9e33fcf4ee43ceefff6a9fd945bc
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-26 22:20:29 +0545

    Create dummy LogIn and CreateAccount component

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,13 @@
+import React, { useState } from 'react';
+
+// components
+import LogIn from 'components/LogIn';
+import CreateAccount from 'components/CreateAccount';
+
 function Home() {
-  return <div>Home</div>;
+  const [isLogginIn] = useState(true);
+
+  return isLogginIn ? <LogIn /> : <CreateAccount />;
 }
 
 export default Home;
