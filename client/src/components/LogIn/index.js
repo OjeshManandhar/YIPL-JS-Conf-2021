9050690e9269fb17b60c35abe9761c2c9d2d8655
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-26 22:47:51 +0545

    Create dummy log in form

--- client/src/components/LogIn/index.js
+++ client/src/components/LogIn/index.js
@@ -1,5 +1,55 @@
+import { useState, useCallback } from 'react';
+
+// styles
+import * as S from './styles';
+
 function LogIn() {
-  return <div>Log In</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const clearForm = useCallback(() => {
+    setEmail('');
+    setPassword('');
+  }, []);
+
+  const submitForm = useCallback(
+    e => {
+      e.preventDefault();
+
+      console.log('data:', { email, password });
+
+      clearForm();
+    },
+    [email, password, clearForm]
+  );
+
+  return (
+    <S.Container>
+      <form onSubmit={submitForm}>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          required
+          autoFocus
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <input
+          id='password'
+          name='password'
+          type='password'
+          required
+          minLength={8}
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <button type='submit'>Log in</button>
+      </form>
+    </S.Container>
+  );
 }
 
 export default LogIn;
---
+++ client/src/components/LogIn/styles.js
@@ -0,0 +1,12 @@
+// packages
+import styled from 'styled-components';
+
+// global
+import * as G from 'global/styles';
+
+export const Container = styled(G.Container)`
+  padding: 1rem 2rem;
+
+  border: 1px solid red;
+  border-radius: 5px;
+`;
