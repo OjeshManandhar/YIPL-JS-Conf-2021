ebe8dbfe505958d078b976bc0f815280bba926ad
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-26 22:38:40 +0545

    Create a splash screen

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // components
 import LogIn from 'components/LogIn';
+import SplashScreen from 'components/SplashScreen';
 import CreateAccount from 'components/CreateAccount';
 
 function Home() {
   const [isLogginIn] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return isLogginIn ? <LogIn /> : <CreateAccount />;
+  useEffect(() => {
+    // Check token
+    setTimeout(() => setIsLoading(false), 2.5 * 1000);
+  });
+
+  return isLoading ? (
+    <SplashScreen />
+  ) : isLogginIn ? (
+    <LogIn />
+  ) : (
+    <CreateAccount />
+  );
 }
 
 export default Home;
