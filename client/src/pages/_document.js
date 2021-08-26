b04f3711e6b95d026b8e7d4c5905275c058a9990
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-26 21:32:45 +0545

    Add Open Sans font

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -1,5 +1,6 @@
-import Document from 'next/document';
+// packages
 import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -25,5 +26,22 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
