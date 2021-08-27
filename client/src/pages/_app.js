a7447328d68f004334f65889ae05f01ee3e9baa7
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-27 16:02:50 +0545

    Setup apollo client

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,6 +1,12 @@
 // Next
 import Head from 'next/head';
 
+// packages
+import { ApolloProvider } from '@apollo/client';
+
+// utils
+import client from 'utils/apollo-client';
+
 // styles
 import 'styles/globals.css';
 
@@ -9,12 +15,12 @@ import { APP_NAME } from 'env_config';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Head>
         <title>{APP_NAME}</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   );
 }
 
