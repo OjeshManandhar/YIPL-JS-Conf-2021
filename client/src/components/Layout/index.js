5a57b4b4b2171fccffe1826f4437eb259cb33c19
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-28 16:59:19 +0545

    Add nav links

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,5 +1,5 @@
 // next
-import Image from 'next/image';
+import Link from 'next/link';
 
 // assets
 import logo from 'public/favicon-32x32.png';
@@ -12,8 +12,17 @@ function Layout(props) {
     <>
       <S.Header>
         <S.NavBar>
-          <Image src={logo} alt='Task Manager' width={32} height={32} />
-          <div>List</div>
+          <S.Image src={logo} alt='Task Manager' width={32} height={32} />
+
+          <S.Links>
+            <Link href='/tasks' passHref>
+              <S.Link>Tasks</S.Link>
+            </Link>
+            <Link href='/projects' passHref>
+              <S.Link>Projects</S.Link>
+            </Link>
+            <S.Logout>Log Out</S.Logout>
+          </S.Links>
         </S.NavBar>
       </S.Header>
       {props.children}
--- client/src/components/Layout/styles.js
+++ client/src/components/Layout/styles.js
@@ -1,3 +1,6 @@
+// next
+import _Image from 'next/image';
+
 // packages
 import styled from 'styled-components';
 
@@ -26,5 +29,43 @@ export const NavBar = styled.nav`
   /* For Image i.e. logo */
   & img {
     cursor: pointer;
+  }
+`;
+
+export const Image = styled(_Image)`
+  cursor: pointer;
+`;
+
+export const Links = styled.ul`
+  padding: 0;
+  list-style-type: none;
+`;
+
+export const Link = styled.a`
+  margin-right: 2rem;
+
+  transition: all 0.3s linear;
+
+  &:hover,
+  &:active {
+    color: var(--dark-sea-green);
+  }
+`;
+
+export const Logout = styled.button`
+  font: inherit;
+  color: inherit;
+
+  border: none;
+  background: none;
+
+  margin-left: 2rem;
+
+  cursor: pointer;
+
+  transition: all 0.3s linear;
+
+  &:hover {
+    color: var(--dark-sea-green);
   }
 `;
