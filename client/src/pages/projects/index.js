f8a19990d6a7f28d5505c730a23df53203971981
OjeshManandhar <ozes.manandhar@gmail.com>
2021-08-29 15:48:59 +0545

    Fetch projects

--- client/src/pages/projects/index.js
+++ client/src/pages/projects/index.js
@@ -1,5 +1,40 @@
+import { useMemo, useEffect } from 'react';
+
+// packages
+import { gql, useQuery } from '@apollo/client';
+
+// components
+import Loading from 'components/Loading';
+
+// styles
+import * as S from './styles';
+
+const GET_PROJECTS = gql`
+  query ListProjects {
+    listProjects {
+      id
+      title
+      description
+      status
+    }
+  }
+`;
+
 function Projects() {
-  return <div>Projects Page</div>;
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+
+  const projects = useMemo(() => data && data.listProjects, [data]);
+
+  useEffect(() => {
+    ``;
+    if (error) {
+      window.alert(error.message);
+    }
+  }, [error]);
+
+  if (loading) return <Loading />;
+
+  return <S.Container>{JSON.stringify(projects, null, 2)}</S.Container>;
 }
 
 export default Projects;
